b2fd135b278e3abb4ed854cd109b9a82d71ec539
vovasik <petrozavodsky@gmail.com>
2018-05-23 22:40:56 +0300

    Hack php 7.2 compatible

--- src/BroYandexZen/Classes/Feed.php
+++ src/BroYandexZen/Classes/Feed.php
@@ -28,6 +28,15 @@ class Feed
 
     public function feed_markup()
     {
+
+        /**
+         * Hack php 7.2 compatible
+         */
+        global $page, $pages;
+        $pages = [1, 2];
+        $page = [1, 2];
+
+
         header('Content-Type: ' . feed_content_type('rss') . '; charset=' . get_option('blog_charset'), true);
         do_action('BroYandexZenFeed_feed_before');
         $template = apply_filters('BroYandexZenFeed_template_rss', $this->plugin_path . "templates/feed.php");
