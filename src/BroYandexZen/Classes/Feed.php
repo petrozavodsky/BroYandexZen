8348f524330917316c8da55bdf3a6054cdfa53fd
vovasik <petrozavodsky@gmail.com>
2018-05-23 21:41:40 +0300

    Fix slug

--- src/BroYandexZen/Classes/Feed.php
+++ src/BroYandexZen/Classes/Feed.php
@@ -3,27 +3,30 @@
 namespace BroYandexZen\Classes;
 
 
-class Feed {
+class Feed
+{
 
-	private $plugin_path;
-	private $slug = 'feed-yandex-zen';
+    private $plugin_path;
+    private $slug;
 
-	function __construct( $plugin_path, $slug ) {
-		$this->plugin_path = $plugin_path;
-		$this->slug        = $slug;
-		add_feed( $slug, [ $this, "add_feed" ] );
-	}
+    function __construct($plugin_path, $slug)
+    {
+        $this->plugin_path = $plugin_path;
+        $this->slug = $slug;
+        add_feed($slug, [$this, "add_feed"]);
+    }
 
 
-	function feed_markup() {
-		header( 'Content-Type: ' . feed_content_type( 'rss' ) . '; charset=' . get_option( 'blog_charset' ), true );
-		do_action( 'BroYandexZenFeed_feed_before' );
-		$template = apply_filters( 'BroYandexZenFeed_template_rss', $this->plugin_path . "templates/feed.php" );
-		if ( file_exists( $template ) ) {
-			require_once( $template );
-		}
-		do_action( 'BroYandexZenFeed_feed_after' );
-		exit;
-	}
+    function feed_markup()
+    {
+        header('Content-Type: ' . feed_content_type('rss') . '; charset=' . get_option('blog_charset'), true);
+        do_action('BroYandexZenFeed_feed_before');
+        $template = apply_filters('BroYandexZenFeed_template_rss', $this->plugin_path . "templates/feed.php");
+        if (file_exists($template)) {
+            require_once($template);
+        }
+        do_action('BroYandexZenFeed_feed_after');
+        exit;
+    }
 
 }
