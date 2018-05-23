89a70c03c4b2ddb6cfcb85ba0cb39da8c83d51eb
vovasik <petrozavodsky@gmail.com>
2018-05-23 22:55:05 +0300

    Clean

--- templates/feed.php
+++ templates/feed.php
@@ -8,53 +8,58 @@
 
     <channel>
         <title>
-			<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+            <?php echo esc_html(get_bloginfo('name')); ?>
         </title>
-        <link><?php echo esc_html( get_option( 'home' ) ); ?></link>
+        <link><?php echo esc_html(get_option('home')); ?></link>
         <description>
-			<?php echo esc_html( get_bloginfo( 'description' ) ); ?>
+            <?php echo esc_html(get_bloginfo('description')); ?>
         </description>
-       <language><?php echo array_shift( explode( "-", get_bloginfo( 'language' ) ) );?></language>
-		<?php
-		global $post;
-		$args  = array(
-			'post_type'      => 'post',
-			'posts_per_page' => 32,
+        <language><?php echo array_shift(explode("-", get_bloginfo('language'))); ?></language>
+        <?php
+
+        global $post;
+
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => 32,
         );
-		$args  = apply_filters( "BroYandexZenFeedFields_post_args", $args );
-		$posts = get_posts( $args );
-		foreach ( $posts as $post ):?>
+
+        $args = apply_filters("BroYandexZenFeedFields_post_args", $args);
+
+        $posts = get_posts($args);
+
+        foreach ($posts as $post):?>
 
             <item>
 
-				<?php do_action( 'BroYandexZenFeedFields_before', $post ); ?>
+                <?php do_action('BroYandexZenFeedFields_before', $post); ?>
 
-                <guid><?php echo esc_html( get_the_ID() ) ?></guid>
+                <guid><?php echo esc_html(get_the_ID()) ?></guid>
 
-                <title><?php echo esc_html( get_the_title( $post ) ); ?></title>
+                <title><?php echo esc_html(get_the_title($post)); ?></title>
 
-                <link><?php echo esc_url( get_the_permalink( $post ) ); ?></link>
+                <link><?php echo esc_url(get_the_permalink($post)); ?></link>
 
-                <pdalink><?php echo esc_url( get_the_permalink( $post ) ); ?></pdalink>
+                <pdalink><?php echo esc_url(get_the_permalink($post)); ?></pdalink>
 
-                <pubDate><?php echo esc_html( mysql2date( 'D, d M Y H:i:s +0000', get_post_time( 'Y-m-d H:i:s', true, $post ), false ) ); ?></pubDate>
+                <pubDate><?php echo esc_html(mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true, $post), false)); ?></pubDate>
 
                 <media:rating scheme="urn:simple">nonadult</media:rating>
 
                 <description>
-                    <![CDATA[<?php echo esc_html( get_the_excerpt( $post ) ); ?>]]>
+                    <![CDATA[<?php echo esc_html(get_the_excerpt($post)); ?>]]>
                 </description>
 
-				<?php do_action( 'BroYandexZenFeedFields_after', $post ); ?>
+                <?php do_action('BroYandexZenFeedFields_after', $post); ?>
 
-				<?php do_action( 'BroYandexZenFeedFields_related', $post, $posts ); ?>
+                <?php do_action('BroYandexZenFeedFields_related', $post, $posts); ?>
 
             </item>
 
-			<?php
-			wp_reset_postdata();
-		endforeach;
-		?>
+            <?php
+            wp_reset_postdata();
+        endforeach;
+        ?>
 
     </channel>
 </rss>
